a9c2eea975206a7385cbcfe4faec30bb869b90c6
rolandobrown <rolando.brown@gmail.com>
2015-08-26 15:27:01 -0400

    fix Metallica incorrectly getting credit for one of the best songs of all time. Edit to Nirvana

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,7 +1,13 @@
 class App < Sinatra::Base
   set :views, Proc.new { File.join(root, '../views')}
+  set :public_folder, Proc.new { File.join(root, '../../public')}
 
   get '/' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
+  get '/songs' do
     @songs = Song.all
     erb :'songs/index'
   end
@@ -10,6 +16,21 @@ class App < Sinatra::Base
     @song = Song.find(params[:id])
     erb :'songs/show'
   end
-  
-end
 
+  get '/songs/:id/edit' do
+  @song = Song.find(params[:id])
+  erb :"songs/edit"
+  end
+
+  post '/songs/:id' do
+    @song = Song.find(params[:id])
+    request = Rack::Request.new(env)
+    @song.title = request.params["song"]["title"]
+    @song.artist = request.params["song"]["artist"]
+    @song.album = request.params["song"]["album"]
+    @song.genre = request.params["song"]["genre"]
+    @song.length = request.params["song"]["length"]
+    erb :'songs/show'
+  end
+
+end
